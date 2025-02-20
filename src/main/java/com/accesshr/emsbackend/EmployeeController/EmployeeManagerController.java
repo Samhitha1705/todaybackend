9b665fc0850a0377d3265b0977c87515274880c5
Samhitha1705 <97188512+Samhitha1705@users.noreply.github.com>
2025-02-20 15:47:28 +0530

    Update EmployeeManagerController.java

--- src/main/java/com/accesshr/emsbackend/EmployeeController/EmployeeManagerController.java
+++ src/main/java/com/accesshr/emsbackend/EmployeeController/EmployeeManagerController.java
@@ -28,7 +28,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/employeeManager")
-@CrossOrigin(origins = "https://webresourcesfront.azurewebsites.net") // Adjust as needed for your frontend
+@CrossOrigin(origins = "https://webresourcesfront-gcbqhpdhcthudca9.canadacentral-01.azurewebsites.net") // Adjust as needed for your frontend
 public class EmployeeManagerController {
 
     @Value("${azure.storage.connection-string}")
